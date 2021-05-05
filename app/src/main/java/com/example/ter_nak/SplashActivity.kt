d434c42be85add4e3ca1adedd9a31e2293ee985d
Wahyu Dwe <wahyu5161.w5@gmail.com>
2021-05-05 09:57:23 +0800

    app final project

---
+++ app/src/main/java/com/example/ter_nak/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.ter_nak
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val pref = getSharedPreferences("PREFERENCE", Context.MODE_PRIVATE)
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
---
+++ app/src/main/java/com/example/ter_nak/produk/ProdukAyambroilerActivity.kt
@@ -0,0 +1,60 @@
+package com.example.ter_nak.produk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.viewpager.widget.ViewPager
+import com.example.ter_nak.R
+import com.example.ter_nak.SliderAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class ProdukAyambroilerActivity : AppCompatActivity() {
+    private var currentPage = 0
+    private var numPages = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_produk_ayambroiler)
+
+        val assets = listOf(
+            R.drawable.photo_one,
+            R.drawable.photo_two,
+            R.drawable.photo_three
+        )
+
+        createSlider(assets)
+    }
+
+    private fun createSlider(string: List<Int>) {
+        vpSlider.adapter = SliderAdapter(this, string)
+        indicator.setViewPager(vpSlider)
+        val density = resources.displayMetrics.density
+        //Set Circle indicator radius
+        indicator.radius = 5 * density
+        numPages = string.size
+        // Auto getData of viewpager
+        val update = Runnable {
+            if (currentPage === numPages) {
+                currentPage = 0
+            }
+            vpSlider.setCurrentItem(currentPage++, true)
+        }
+        val swipeTimer = Timer()
+        swipeTimer.schedule(object : TimerTask() {
+            override fun run() {
+                Handler(Looper.getMainLooper()).post(update)
+            }
+        }, 5000, 5000)
+        // Pager listener over indicator
+        indicator.setOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageSelected(position: Int) {
+                currentPage = position
+            }
+
+            override fun onPageScrolled(pos: Int, arg1: Float, arg2: Int) {}
+            override fun onPageScrollStateChanged(pos: Int) {}
+        })
+    }
+}
